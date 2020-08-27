ae72f79f3375d4109bab78730469a5b0a3cf54e9
Katie <kayoung7766@gmail.com>
2020-08-27 18:44:23 -0500

    files created

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/??)
+
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    
+    return render_template("index.html", ??=??)
+
