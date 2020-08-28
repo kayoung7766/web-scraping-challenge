5c685f604bc03fe420df07c5623c58193a63ed71
Katie <kayoung7766@gmail.com>
2020-08-27 20:31:26 -0500

    through most of the app

--- app.py
+++ app.py
@@ -6,13 +6,36 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/??)
+#What do I put after localhost?
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
 
-    
-    return render_template("index.html", ??=??)
+    mars_dataDB = mongo.db.mars_data_collected.find_one()
+
+    #Return the tempalte and dadata
+
+    return render_template("index.html", planet = mars_dataDB)
+
+@app.route("/scrape_all")
+def scrape():
+
+    mars_dataDB = mongo.db.mars_data_collected
+
+    mars_data = scrape_mars.scrape_all()
+
+    #Update the mongo datase using update and upsert=True
+
+    mars_dataDB.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+
+    return redirect("/", code = 302)
+
+if __name__ == "__main__":
+    app.run(debug=True, port = 5020)
+
 
--- scrape_mars.py
+++ scrape_mars.py
@@ -3,37 +3,103 @@ from bs4 import BeautifulSoup as bs
 import requests
 from splinter import Browser
 import os
-def scrape_info():
+import datetime as dt
+import time
+
+
+def home():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    return Browser('chrome', **executable_path, headless=False)
+
+
+def scrape_all():
+
+    browser = home()
+
+    title, news = scrape_news()
+
+    #Run all other functions 
+    mars_data = {
+        "title": title,
+        "paragraph": news,
+        "main_image": scrape_main_image(),
+        "html_table1": mars_table(),
+        "hemisphere_image_urls": mars_hemis()
+    }
+
+    browser.quit()
+
+    return mars_data
+
+
+def scrape_news():
+
+    browser = home()
+
+
     #scraping news paragraph and title
     url="https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
+    
     browser.visit(url)
     browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
     html=browser.html
     soup = bs(html, 'html.parser')
     result = soup.select_one("ul.item_list li.slide")
-    title=result.find("div", class_= "content_title").get_text()
-    news=result.find("div", class_ = "article_teaser_body").get_text()
-    #scraping featured image url
-    executable_path = {'executable_path': 'chromedriver.exe'}
-    browser = Browser('chrome', **executable_path, headless=False)
+
+    try:
+        title=result.find("div", class_= "content_title").get_text()
+        news=result.find("div", class_ = "article_teaser_body").get_text()
+
+    except AttributeError:
+        return None, None
+    
+    browser.quit()
+
+    return title, news
+
+def scrape_main_image():
+    browser = home()
+    
     urljpl = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
     browser.visit(urljpl)
+
+    time.sleep(1)
+
     html=browser.html
     soup = bs(html, 'html.parser')
-    result=soup.find("div", class_ = "carousel_items")
-    image=result.find("a", class_ = "button fancybox")
-    link=(image['data-fancybox-href'])
-    full_link=urljpl+link
-    full_link
+    try:
+        result=soup.find("div", class_ = "carousel_items")
+        image=result.find("a", class_ = "button fancybox")
+        link=(image['data-fancybox-href'])
+        full_link=urljpl+link
+    except AttributeError:
+        return None
+    
+
+    browser.quit()
+
+    return full_link
+
+def mars_table():
+
+    browser = home()
+
     #using pandas to get Mars facts
     url3="https://space-facts.com/mars/"
     tables = pd.read_html(url3)
     table1=tables[0]
-    table2=tables[1]
+    #table2=tables[1]
     html_table1 = table1.to_html()
-    html_table2=table2.to_html()
+    #html_table2=table2.to_html()
+
+
+    browser.quit()
+
+    return html_table1
+
+def mars_hemis():
+
+    browser = home()
     #scraping 4 hemisphere urls
     url4="https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     executable_path = {'executable_path': 'chromedriver.exe'}
@@ -93,20 +159,12 @@ def scrape_info():
     valles_marineris_link="https://astrogeology.usgs.gov"+link_partial
     # creating a dictionary
     hemisphere_image_urls = [
-        {"title": "Valles Marineris Hemisphere", "img_url": "https://astrogeology.usgs.gov/cache/images/b3c7c6c9138f57b4756be9b9c43e3a48_valles_marineris_enhanced.tif_full.jpg"},
-        {"title": "Cerberus Hemisphere", "img_url": "https://astrogeology.usgs.gov/cache/images/f5e372a36edfa389625da6d0cc25d905_cerberus_enhanced.tif_full.jpg"},
-        {"title": "Schiaparelli Hemisphere", "img_url": "https://astrogeology.usgs.gov/cache/images/3778f7b43bbbc89d6e3cfabb3613ba93_schiaparelli_enhanced.tif_full.jpg"},
-        {"title": "Syrtis Major Hemisphere", "img_url": "https://astrogeology.usgs.gov/cache/images/555e6403a6ddd7ba16ddb0e471cadcf7_syrtis_major_enhanced.tif_full.jpg"},
-    ]
-    #Store data into dictionary?
-    mars_data = {
-        "title": title,
-        "paragraph": news,
-        "full_link": full_link,
-        "html_table1": html_table1,
-        "html_table2": html_table2,
-        "hemisphere_image_urls": hemisphere_image_urls
+    {"title": "Valles Marineris Hemisphere", "img_url": valles_marineris_link},
+    {"title": "Cerberus Hemisphere", "img_url": cerberus_link},
+    {"title": "Schiaparelli Hemisphere", "img_url": schiaparelli_link},
+    {"title": "Syrtis Major Hemisphere", "img_url": syrtis_major_link},
+        ]
+    
+    browser.quit()
 
-    }
-
-    return mars_data 
+    return hemisphere_image_urls 
